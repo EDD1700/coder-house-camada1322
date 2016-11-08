09efe0bcb2eca182b5bac48b5aaf3a37406e78c0
EDD1700 <gonzalez.eduardo.javier@gmail.com>
2016-11-08 05:08:35 -0300

    agregados los tps

---
+++ trabajo final/Freenom - Un nombre para todo el mundo_files/loggedon.php
@@ -0,0 +1,13 @@
+
+$(function() {
+    var $loggedon = 0;
+    var $name     = "";
+
+    if (!$loggedon) {
+        $(".hide-my-freenom").show();
+    } else {
+        $(".hide-my-freenom").hide();
+        $(".my-freenom").show();
+        $("#username").html($name);
+    }
+});
